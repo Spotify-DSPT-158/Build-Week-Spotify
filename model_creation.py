3abc7e3618a07492e42452a655a381c4c853de9a
jeremy <jeremy.jewett@gmail.com>
2021-01-06 21:49:44 -0500

    Finished base product to be renamed shortly...

--- model_creation.py
+++ model_creation.py
@@ -14,4 +14,9 @@ with open(df_path, 'rb') as f:
 
 def find_recommended_songs(track_id):
     song_index = df.index[df['id'] == track_id]
-    audio_features = df
+    audio_features = df.iloc[song_index, 3:].to_numpy()
+    distances, indices = model.kneighbors(audio_features)
+    return df.iloc[indices[0], 'id'].tolist()
+
+def track_id_in_df(track_id):
+    return len(df.index[df['id'] == track_id])> 0
